0e953b86475ab5f8f39d151f6ec1f7e2ae320fe4
md afjal <afjal742000@gmail.com>
2025-04-08 17:59:06 +0530

    ai resume builder

---
+++ components/header.jsx
@@ -0,0 +1,97 @@
+import {
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  SignUpButton,
+  UserButton,
+} from "@clerk/nextjs";
+import Link from "next/link";
+import React from "react";
+import { Button } from "./ui/button";
+import { ChevronDown, FileText, GraduationCap, LayoutDashboard, PenBox, StarsIcon } from "lucide-react";
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "./ui/dropdown-menu";
+
+const Header = () => {
+  return (
+    <header
+      className="fixed top-0 w-full border-b bg-background/80 backdrop-blur-md z-50 
+       supports-[backdrop-filter]:bg-background/60"
+    >
+      <nav className="container mx-auto px-4 flex h-16 items-center justify-between">
+        <Link href="/">
+          <img
+            src="/logo.png"
+            alt="AI Resume Builder"
+            width={200}
+            height={100}
+            className="h-16 mt-4 py-1 w-auto object-contain"
+          />
+        </Link>
+
+        <div className="flex items-center space-x-4">
+          <SignedIn>
+            <Link href={"/dashboard"}>
+              <Button variant={"outline"}>
+                <LayoutDashboard className="mr-2 h-4 w-4" />
+                <span className="hidden md:block">Industry Insights</span>
+              </Button>
+            </Link>
+          </SignedIn>
+
+          <DropdownMenu>
+            <DropdownMenuTrigger>
+            <Button>
+                <StarsIcon className=" h-4 w-4" />
+                <span className="hidden md:block">Growth Tools</span>
+                <ChevronDown className=" h-4 w-4" />
+            </Button>
+            </DropdownMenuTrigger>
+
+            <DropdownMenuContent>
+              <DropdownMenuItem>
+                <Link href={'/resume'} className="flex items-center gap-2">
+                <FileText className=" h-4 w-4" />
+                <span>Build Resume</span>
+                </Link>
+              </DropdownMenuItem>
+
+              <DropdownMenuItem>
+              <Link href={'/ai-cover-letter'} className="flex items-center gap-2">
+                <PenBox className=" h-4 w-4" />
+                <span>Cover Letter</span>
+                </Link>
+              </DropdownMenuItem>
+
+              <DropdownMenuItem>
+              <Link href={'/interview'} className="flex items-center gap-2">
+                <GraduationCap className="h-4 w-4" />
+                <span>Interview Prep</span>
+                </Link>
+              </DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+
+                <SignedOut>
+                    <SignInButton />
+                      <Button variant="outline">Sign In</Button>
+                    <SignUpButton />
+                </SignedOut>
+
+                <SignedIn>
+                    <UserButton 
+                    appearance={{
+                        elements:{
+                            avatarBox: "h-10 w-10",
+                            userButtonPopoverCard: "shadow-xl",
+                            userPreviewMainIdentifier: "font-semibold",
+                        }
+                    }}
+                    />
+                </SignedIn>
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
